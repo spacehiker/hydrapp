f429367a4713aa81bd360219c4b86dbd17163f9f
spacehiker <filip.osin@gmail.com>
2020-03-02 23:27:45 +0100

    feat: initial version

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// service worker registration - remove if you're not going to use it
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+// place your code below
+
+
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const value = document.querySelector('.counter__value--js');
+const key = new Date().toISOString().slice(0, 10);
+
+if (!localStorage.getItem(key)) {
+    localStorage.setItem(key, 0);
+    value.innerHTML = '0';
+} else {
+    value.innerHTML = localStorage.getItem(key);
+}
+
+buttonAdd.addEventListener('click', (e) => {
+    localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+    value.innerHTML = localStorage.getItem(key);
+})
+
+buttonRemove.addEventListener('click', (e) => {
+    const currentValue = parseInt(localStorage.getItem(key));
+    if (value.innerHTML > 0) {
+        localStorage.setItem(key, parseInt(localStorage.getItem(key)) - 1);
+        value.innerHTML = localStorage.getItem(key);
+    }
+})
